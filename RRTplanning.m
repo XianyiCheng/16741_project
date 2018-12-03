143a63fcb7fb5eaa2e490c9541c55f9edb509a63
Chia Dai <chiad@CHIA.SP.CS.CMU.EDU>
2018-12-02 21:09:23 -0500

    modified RRT planning

--- RRTplanning.m
+++ RRTplanning.m
@@ -1,49 +1,52 @@
-function [T, isfound] = RRTplanning(Xstart, Xgoal, env, object, maxIter, thr)
-% T: tree of RTT planning
-% Xstart: start configuration of object, [x,y,theta]
-% Xgoal: goal configuration of object, only [theta]
-% maxIter: maximum iteration number
-% thr: the threshold allow for Xgoal
+function [T_, isfound] = RRTplanning(Xstart, Xgoal, env, object, maxIter, thr)
+    % T: tree of RTT planning
+    % Xstart: start configuration of object, [x,y,theta]
+    % Xgoal: goal configuration of object, only [theta]
+    % maxIter: maximum iteration number
+    % thr: the threshold allow for Xgoal
 
-%
-isfound = 0;
-[isStartOk,start_contacts] = CollisionDetection(env, object, Xstart);
-if ~isStartOk 
-    error('start configuration collided!');
-end
-if isempty(start_contacts)
-    error('The object is floating :-) ');
-end
-
-%Start to construct RTT tree
-T = RTTtree(X_start, start_contacts, []);
-
-for i = 1:maxIter
-    if 1% TODO: check if goal is reached
-        isfound = 1;
-        break;
+    %
+    isfound = 0;
+    end_ind = 0;
+    [isStartOk,start_contacts] = CollisionDetection(env, object, Xstart);
+    if ~isStartOk 
+        error('start configuration collided!');
     end
-    
-    Xrand = []; % TODO: sample from random state,50% from the goal state
-    Xnear = T.nearestNeighbor(Xrand); %TODO: in RRT tree class
-    Xnew = extend(Xrand, Xnear); % TODO
-    [isXnewOk,Xnew_env_contacts] = CollisionDetection(env, object, Xnew);
-    if ~isXnewOk
-        continue;
+    if isempty(start_contacts)
+        error('The object is floating :-) ');
     end
-    
-    [isXnewMotion, Xnew_finger_contacts] = isStableMotionAllowed( ) % TODO
-    
-    if ~isXnewMotion
-        continue;
+    %Start to construct RTT tree
+    T = RRTtree(Xstart, start_contacts, [])
+
+    for i = 1:maxIter
+        Xrand = []; % TODO: sample from random state,50% from the goal state
+        [Xnear_ind, ~] = T.nearestNeighbor(T, Xrand); %TODO: in RRT tree class
+        Xnear = [T.vertex(Xnear_ind).x, T.vertex(Xnear_ind).y, T.vertex(Xnear_ind).theta]
+        Xnew = extend(Xrand, Xnear); % TODO
+        [isXnewOk,Xnew_env_contacts] = CollisionDetection(env, object, Xnew);
+        if ~isXnewOk
+            continue;
+        end
+        
+        [isXnewMotion, Xnew_finger_contacts] = isStableMotionAllowed( ) % TODO
+        
+        if ~isXnewMotion
+            continue;
+        end
+        
+        % combining adding vertex and edge
+        T = T.add_node(T, Xnear_ind, Xnew, Xnew_env_contacts, Xnew_finger_contacts);
+        [Xclosest_ind, cur_dist] = T.earestNeighbor(T, Xgoal)
+        if cur_dist < thr % TODO: check if goal is reached
+            isfound = 1;
+            end_ind = Xclosest_ind;
+            break;
+        end
     end
-    
-    T.addVertex(Xnew, Xnew_env_contacts, Xnew_finger_contacts); %TODO
-    T.addEdge(Xnear, Xnew); %TODO
-    
-    
+    if end_ind ~= 0
+        % display states and fingers
+        T.get_path(T, end_ind);
+        T_ = T;
+    else
+        error('no state close enough to goal state');
 end
-
-    
-
-
