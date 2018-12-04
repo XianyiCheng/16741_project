a1498d8220b37f3d025ff3c34a391e659d722aa0
tsaiJN <athaha821216@gmail.com>
2018-12-04 17:58:49 -0500

    remove duplicate contacts whose distance to each other is less than 1e-3

--- CollisionDetectionV2.m
+++ CollisionDetectionV2.m
@@ -5,5 +5,17 @@ bCD = bCD || b;
 contacts = unique([contacts, c]', 'rows', 'stable')';
 if ~isempty(contacts)
     bCD = 0;
+    ret = contacts(:,1);
+    for i=2:size(contacts,2)
+        flag = false;
+        for j=1:size(ret, 2)
+            if norm(contacts(:,i)-ret(:,j)) <= 1e-3
+                flag = true;
+            end
+        end
+        if flag
+            ret = [ret, contacts(:,i)];
+        end
+    end
 end
 end
